693b6d994e63844159568b4bacb80ad15e3431ab
asrulsibaoel <asrulsibaoel@gmail.com>
2017-08-12 17:51:36 +0700

    added: exception handlers

---
+++ src/Domain/User/Exception/FailedToCreateUserException.js
@@ -0,0 +1,11 @@
+export class FailedToCreateUserException extends Error {
+
+    constructor(message) {
+        super(message);
+        if(typeof message == "string") {
+            this.message = message;
+        } else {
+            throw new Error('message parameter must be string');
+        }
+    }
+}
